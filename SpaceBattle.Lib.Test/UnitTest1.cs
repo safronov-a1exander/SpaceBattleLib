0ba90a52ee3c961f49d4701fa5a01eaf0ed05379
Cheburilla <95407508+Cheburilla@users.noreply.github.com>
2022-11-02 09:23:41 +0600

    Update UnitTest1.cs

--- SpaceBattle.Lib.Test/UnitTest1.cs
+++ SpaceBattle.Lib.Test/UnitTest1.cs
@@ -48,139 +48,62 @@ public class UnitTest1
 
 public class UnitTest2 {
     [Fact]
-    public void PosTest_CreateVec(){
-        Vector t = new Vector(1, 1);
-        Assert.NotNull(t);
+    public void PosTest_CreateAngle()
+    {
+        Assert.IsType<Angle>(new Angle(60, 1));
     }
 
     [Fact]
-    public void NegTest_CreateVec(){
-        Assert.Throws<ArgumentException>(() => new Vector());
+    public void NegTest_CreateAngle()
+    {
+        Assert.Throws<DivideByZeroException>(() => new Angle(45, 0));
+    }
+
+    [Fact]
+    public void PosTest_AngleEq()
+    {
+        Angle a = new Angle(45, -1);
+        Angle b = new Angle(-135, 3);
+        Assert.True(a == b);
+    }
+
+    [Fact]
+    public void Test_AngleEqMethod()
+    {
+        Angle a = new Angle(-30, -1);
+        int b = 1;
+        Assert.False(a.Equals(b));
     }
     
     [Fact]
-    public void Test_VectorToStr(){
-        var obj = new Vector(12, 24);
-        Assert.Equal("Vector (12, 24)", obj.ToString());
+    public void NegTest_AngleEq()
+    {
+        Angle a = new Angle(45, -1);
+        Angle b = new Angle(-135, 45);
+        Assert.False(a == b);
     }
-
+    
     [Fact]
-    public void PosTest_VectorInd(){
-        var obj = new Vector(1, 2);
-        obj[1] += obj[0];
-        Assert.Equal(new Vector(1, 3), obj);
+    public void Test_AngleNotEq()
+    {
+        Angle a = new Angle(45, -1);
+        Angle b = new Angle(-135, 45);
+        Assert.True(a != b);
     }
-
+    
     [Fact]
-    public void NegTest_VectorInd(){
-        var obj = new Vector(1, 2);
-        var act = () => obj[1] += obj[2];
-        act.Should().Throw<IndexOutOfRangeException>();
+    public void Test_AngleAdd()
+    {
+        Angle a = new Angle(45, 1);
+        Angle b = new Angle(90, 1);
+        Assert.Equal(new Angle(135, 1), a + b);
     }
-
+    
     [Fact]
-    public void PosTest_VectorEq(){
-        var obj1 = new Vector(1, 2);
-        var obj2 = new Vector(1, 2);
-        Assert.True(obj1 == obj2);
-    }
-
-    [Fact]
-    public void NegTest_VectorEq1(){
-        var obj1 = new Vector(1, 3);
-        var obj2 = new Vector(1, 2);
-        Assert.True(obj1 != obj2);
-    }
-
-    [Fact]
-    public void NegTest_VectorEq2(){
-        var obj1 = new Vector(1, 3, 4);
-        var obj2 = new Vector(1, 2);
-        Assert.False(obj1 == obj2);
-    }
-
-    [Fact]
-    public void NegTest_VectorEq3(){
-        var obj1 = new Vector(1, 2);
-        var obj2 = new Vector(1, 2);
-        Assert.False(obj1 != obj2);
-    }
-
-    [Fact]
-    public void Test_VectorHash(){
-        var obj1 = new Vector(1, 3);
-        Assert.IsType<int>(obj1.GetHashCode());
-    }
-
-    [Fact]
-    public void PosTest_VectorLess1(){
-        var obj1 = new Vector(1, 2);
-        var obj2 = new Vector(1, 3);
-        Assert.True(obj1 < obj2);
-    }
-
-    [Fact]
-    public void PosTest_VectorLess2(){
-        var obj1 = new Vector(1, 2);
-        var obj2 = new Vector(1, 2, 3);
-        Assert.True(obj1 < obj2);
-    }
-
-    [Fact]
-    public void NegTest_VectorLess1(){
-        var obj1 = new Vector(1, 2);
-        var obj2 = new Vector(1, 2);
-        Assert.False(obj1 < obj2);
-    }
-
-    [Fact]
-    public void NegTest_VectorLess2(){
-        var obj1 = new Vector(1, 2, 3);
-        var obj2 = new Vector(1, 2);
-        Assert.False(obj1 < obj2);
-    }    
-
-    [Fact]
-    public void NegTest_VectorLess3(){
-        var obj1 = new Vector(2, 2);
-        var obj2 = new Vector(1, 2);
-        Assert.False(obj1 < obj2);
-    }
-
-    [Fact]
-    public void PosTest_VectorMore1(){
-        var obj1 = new Vector(1, 3);
-        var obj2 = new Vector(1, 2);
-        Assert.True(obj1 > obj2);
-    }
-
-    [Fact]
-    public void PosTest_VectorMore2(){
-        var obj1 = new Vector(1, 2, 3);
-        var obj2 = new Vector(1, 2);
-        Assert.True(obj1 > obj2);
-    }
-
-    [Fact]
-    public void PosTest_VectorSubstr(){
-        var obj1 = new Vector(1, 2, 3);
-        var obj2 = new Vector(1, 2, 1);
-        var obj3 = obj1 - obj2;
-        Assert.Equal(new Vector(0, 0, 1), obj3);
-    }
-
-    [Fact]
-    public void NegTest_VectorSubstr1(){
-        var obj1 = new Vector(1, 2);
-        var obj2 = new Vector(1, 2, 1);
-        var act = () => obj1 -= obj2;
-        act.Should().Throw<ArgumentException>();
-    }
-
-    [Fact]
-    public void PosTest_VectorMult(){
-        var obj1 = new Vector(1, 2, 3);
-        var obj2 = 3 * obj1;
-        Assert.Equal(new Vector(3, 6, 9), obj2);
+    public void Test_AngleHash()
+    {
+        Angle a = new Angle(-45, 1);
+        Angle b = new Angle(135, -3);
+        Assert.True(a.GetHashCode() == b.GetHashCode());
     }
 }
