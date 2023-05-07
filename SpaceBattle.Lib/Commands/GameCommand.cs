2e4dfb25fc388e8f6c24572a3787f1486dbb4059
Alexander Safronov <sanya.safron2003@gmail.com>
2023-05-07 23:40:58 +0600

    rebase moment

--- SpaceBattle.Lib/Commands/GameCommand.cs
+++ SpaceBattle.Lib/Commands/GameCommand.cs
@@ -1,14 +1,35 @@
+using System.Diagnostics;
+using Hwdtech;
 namespace SpaceBattle.Lib;
 
 public class GameCommand : ICommand
 {
-    public GameCommand()
-    {
+    Queue<ICommand> queue = new Queue<ICommand>(100);
 
+    object scope;
+    public GameCommand(string id, object scope)
+    {
+        this.scope = scope;
     }
 
     public void Execute()
     {
-        
+        var sw = new Stopwatch();
+        IoC.Resolve<ICommand>("Scope.Current.Set", scope).Execute();
+        var quant = IoC.Resolve<TimeSpan>("GetQuant");
+        while(sw.Elapsed < quant)
+        {
+            sw.Start();
+            var success = queue.TryDequeue(out var cmd);
+            try
+            {               
+                if (success) cmd!.Execute();
+                sw.Stop();
+            }
+            catch (Exception e)
+            {
+                IoC.Resolve<ICommand>("Handler.Exception", e, cmd!);
+            }
+        }
     }
 }
