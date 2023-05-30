5fb00db7ad8aacda156e0265cdbe39a45c4034b7
Alexander Safronov <sanya.safron2003@gmail.com>
2023-05-30 12:29:48 +0600

    modified: SpaceBattle.Lib.Test/StrategiesTests/BuilderTests.cs

--- SpaceBattle.Lib.Test/StrategiesTests/BuilderTests.cs
+++ SpaceBattle.Lib.Test/StrategiesTests/BuilderTests.cs
@@ -5,36 +5,7 @@ public class BuilderTests
     [Fact]
     public void PositiveBuilderTest()
     {
-        string header = "namespace SpaceBattle.Lib;public class IMovableAdapter : IMovable";
-
-        string openFigBracket = "{";
-        string closingFigBracket = "}";
-
-        string targetField = "Dictionary<string, object> target;";
-        string constructor = "public IMovableAdapter(object _target){target = (Dictionary<string, object>) _target;}";
-
-        string speedHeader = "public SpaceBattle.Lib.Vector Speed";
-        string speedGetMethod = "get => (SpaceBattle.Lib.Vector) target[\"Speed\"];";
-
-        string coordsHeader = "public SpaceBattle.Lib.Vector Coords";
-        string coordsGetMethod = "get => (SpaceBattle.Lib.Vector) target[\"Coords\"];";
-        string coordsSetMethod = "set => target[\"Coords\"] = value;";
-
-        string expected = 
-        header + 
-        openFigBracket + 
-        targetField + 
-        constructor + 
-        speedHeader + 
-        openFigBracket + 
-        speedGetMethod + 
-        closingFigBracket + 
-        coordsHeader + 
-        openFigBracket + 
-        coordsGetMethod + 
-        coordsSetMethod + 
-        closingFigBracket + 
-        closingFigBracket;
+        string expected = "namespace SpaceBattle.Lib;public class IMovableAdapter : IMovable{Dictionary<string, object> target;public IMovableAdapter(object _target){target = (Dictionary<string, object>) _target;}public SpaceBattle.Lib.Vector Speed{get => (SpaceBattle.Lib.Vector) target[\"Speed\"];}public SpaceBattle.Lib.Vector Coords{get => (SpaceBattle.Lib.Vector) target[\"Coords\"];set => target[\"Coords\"] = value;}}";
 
         Type? IMovableType = Type.GetType("SpaceBattle.Lib.IMovable, SpaceBattle.Lib", true, true);
 
