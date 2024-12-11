2161e61cc2b65e807d52aebebfb2f56f2d6aaaa2
Alexander Safronov <sanya.safron2003@gmail.com>
2024-12-11 10:14:14 +0600

    feat: add grpc router builder and game loader

--- SpaceBattle.Lib.gRPC/Main.cs
+++ SpaceBattle.Lib.gRPC/Main.cs
@@ -1,6 +1,15 @@
-namespace SpaceBattleGrpc;
+namespace SpaceBattle.Lib.gRPC;
+using Grpc.Core;
 
 class SpaceBattleGrpcApp
 {
-    public static void Main(string[] args) { }
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddSingleton(new EndpointGameService((IRouter) ((new CreateEndpointGameStrategy()).Execute(2))));
+        builder.Services.AddGrpc();
+        var app = builder.Build();
+        app.MapGrpcService<EndpointGameService>();
+        app.Run();
+    }
 }
