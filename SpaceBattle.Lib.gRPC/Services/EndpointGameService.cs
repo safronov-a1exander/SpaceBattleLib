db44fa356d0ce21fc206bebb84ce527d70be6a7f
Alexander Safronov <sanya.safron2003@gmail.com>
2024-12-11 10:12:48 +0600

    feat: add endpoint

---
+++ SpaceBattle.Lib.gRPC/Services/EndpointGameService.cs
@@ -0,0 +1,35 @@
+using Grpc.Core;
+using System.Collections.Concurrent;
+using Hwdtech;
+
+namespace SpaceBattle.Lib.gRPC;
+
+public class EndpointGameService : Endpoint.EndpointBase
+{
+    private IRouter _router;
+
+    public EndpointGameService(IRouter router)
+    {
+        _router = router;
+    }
+
+    public override Task<CommandReply> MessageReceiver(CommandRequest request, ServerCallContext context)
+    {
+        int status = 500;
+
+        Dictionary<string, object> data = (Dictionary<string, object>)ProtobufMapperStrategy.Execute(request.Args);
+
+        string gameId = request.Gid;
+        string command = request.Command;
+
+        if (_router.Route(gameId, command, data))
+        {
+            status = 200;
+        }
+
+        return Task.FromResult(new CommandReply
+        {
+            Statuscode = status
+        });
+    }
+}
